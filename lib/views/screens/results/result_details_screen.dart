d024e9ba0546951b155e453ce76135ef773e77b0
HWSEO-AKINOKAMI-53183 <hwseo-akinokami-53183@admindeMac-mini.local>
2024-07-09 13:19:46 +0800

    phk

--- lib/views/screens/results/result_details_screen.dart
+++ lib/views/screens/results/result_details_screen.dart
@@ -1,7 +1,4 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:live_score/controller/match_detail_controller.dart';
 import 'package:live_score/views/widgets/custom_text.dart';
